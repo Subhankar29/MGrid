bbc7f51110da3c3b54fd79e53a18a46f06457dfc
subhankar shah <subhankar29shah@gmail.com>
2026-01-06 19:03:45 -0800

    Formatting the code for better readability.

--- main.cpp
+++ main.cpp
@@ -1,47 +1,51 @@
-#include <iostream>
-#include "MGrid/MGrid/MGrid.h"
-#include "Query/Query.h"
-#include "Query/MGridQuery.h"
 #include "GenerateData/DataSetGenerator.h"
+#include "MGrid/MGrid/MGrid.h"
+#include "Query/MGridQuery.h"
+#include "Query/Query.h"
 #include <chrono>
+#include <iostream>
 
 using namespace std::chrono;
 
 int main() {
 
-    // Generate the data set:
-    DataSetGenerator dataSetGenerator(10000, 64);
+  // Generate the data set:
+  DataSetGenerator dataSetGenerator(5000, 32);
 
-    // Step 1: Get Data set
-    vector<vector<double>> metricObjects = dataSetGenerator.generateDataNonUniformDistribution();
+  // Step 1: Get Data set
+  vector<vector<double>> metricObjects =
+      dataSetGenerator.generateDataNonUniformDistribution();
 
-    // Step 2: Determine the number of Pivots and number of rings
-    long numberOfPivots = 4;
-    long numberOfRings = 5;
-    long queryIndex = 100;
-    long numberOfClusters = 20;
+  // Step 2: Determine the number of Pivots and number of rings
+  long numberOfPivots = 4;
+  long numberOfRings = 5;
+  long queryIndex = 100;
+  long numberOfClusters = 20;
 
-    auto start = std::chrono::high_resolution_clock::now();
+  auto start = std::chrono::high_resolution_clock::now();
 
-    // Step 3: DataSetGenerator MGrid
-    MGrid mGrid(metricObjects, numberOfPivots, numberOfRings, queryIndex, numberOfClusters);
+  // Step 3: DataSetGenerator MGrid
+  MGrid mGrid(metricObjects, numberOfPivots, numberOfRings, queryIndex,
+              numberOfClusters);
 
-    auto end = std::chrono::high_resolution_clock::now();
+  auto end = std::chrono::high_resolution_clock::now();
 
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+  auto duration =
+      std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
 
-    cout << "Time takes to run the whole query " << duration.count() << " ms" << endl;
+  cout << "Time takes to run the whole query " << duration.count() << " ms"
+       << endl;
 
-    vector<vector<double>> pivots = mGrid.getPivots();
+  vector<vector<double>> pivots = mGrid.getPivots();
 
-    vector<Cluster> clusters = mGrid.getCluster();
+  vector<Cluster> clusters = mGrid.getCluster();
 
-    // Step 4: Initialize the MGrid Query Object
-    Query* mGridQuery = new MGridQuery(mGrid);
+  // Step 4: Initialize the MGrid Query Object
+  Query *mGridQuery = new MGridQuery(mGrid);
 
-    // Step 5: Insert Objects to the MGrid:
-    MetricObject objectToBeInserted;
-    mGridQuery->insert(objectToBeInserted);
+  // Step 5: Insert Objects to the MGrid:
+  MetricObject objectToBeInserted;
+  mGridQuery->insert(objectToBeInserted);
 
-    return 0;
+  return 0;
 }
